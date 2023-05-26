02b1d353eb40c030b20def1db4602e917bb57f0f
WeydsonCristiano <weydsoncristiano@hotmail.com>
2023-05-26 13:48:30 -0300

    refatoracao req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,16 +1,36 @@
-def study_schedule(periodo_permanencia, hora_alvo):
-    if not hora_alvo:
-        return None
-
-    if not all(
-        isinstance(periodo, tuple) and len(periodo) == 2
-        for periodo in periodo_permanencia
+def is_valid_period(period):
+    if (
+        isinstance(period, tuple)
+        and len(period) == 2
+        and isinstance(period[0], int)
+        and isinstance(period[1], int)
     ):
+        start_time, end_time = period
+        if start_time <= end_time:
+            return True
+        else:
+            print(
+                f"Invalid period: {period}."
+            )
+    else:
+        print(
+            f"Invalid period: {period}. "
+        )
+    return False
+
+
+def study_schedule(permanence_periods, target_time):
+    if not all(is_valid_period(period) for period in permanence_periods):
         return None
 
-    contador = 0
-    for periodo in periodo_permanencia:
-        if periodo[0] <= hora_alvo <= periodo[1]:
-            contador += 1
+    permanence_periods.sort(key=lambda period: period[1])
+    dp = [float("inf")] * (target_time + 1)
+    dp[0] = 0
 
-    return contador
+    for period in permanence_periods:
+        start_time, end_time = period
+        for time in range(start_time, target_time + 1):
+            if dp[time - start_time] < float("inf"):
+                dp[time] = min(dp[time], dp[time - start_time] + 1)
+
+    return dp[target_time] if dp[target_time] != float("inf") else None
