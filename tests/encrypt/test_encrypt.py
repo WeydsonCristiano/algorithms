40602114eaebb8cb3438eef763ced3dbaff33d2b
WeydsonCristiano <weydsoncristiano@hotmail.com>
2023-05-26 14:55:37 -0300

    refactore: encrypt

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,21 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message("Cristiano", "0")
+
+    with pytest.raises(TypeError):
+        encrypt_message(0, 0)
+
+    message = "Sambatech"
+    key = len(message) // 2
+    output = "etabmaScha"
+    assert encrypt_message(message, key) == output
+
+    key_big = 1000
+    correct_message = "paralelepipedo"
+    output_expected = "odidepilelalap"
+
+    assert encrypt_message(correct_message, key_big) == output_expected
