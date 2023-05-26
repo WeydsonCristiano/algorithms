2f568f20e68d6d4073d37994f313c1af1c49f0b6
WeydsonCristiano <weydsoncristiano@hotmail.com>
2023-05-26 10:46:49 -0300

    feat: req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,16 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
-## iniciando o projeto 
+def study_schedule(periodo_permanencia, hora_alvo):
+    if not hora_alvo:
+        return None
+
+    if not all(
+        isinstance(periodo, tuple) and len(periodo) == 2
+        for periodo in periodo_permanencia
+    ):
+        return None
+
+    contador = 0
+    for periodo in periodo_permanencia:
+        if periodo[0] <= hora_alvo <= periodo[1]:
+            contador += 1
+
+    return contador
