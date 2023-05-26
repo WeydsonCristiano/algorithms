256da326cf91ec97b5f96dc4b5db027409e4e15d
WeydsonCristiano <weydsoncristiano@hotmail.com>
2023-05-26 14:53:24 -0300

    feat: duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) < 2 or not all(
+        isinstance(num, int) and num >= 0 for num in nums
+    ):
+        return False
+
+    unique_values = set()
+    for num in nums:
+        if num in unique_values:
+            return num
+        unique_values.add(num)
+
+    return False
